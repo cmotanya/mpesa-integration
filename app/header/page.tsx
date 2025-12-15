26b523c74ed90a0f9e522a3cb9b08023fbda6cc9
Cornelius Motanya <motanyac@gmail.com>
2025-12-15 07:59:53 +0300

    feat: wrap header and landing page elements in Fade component for improved animations

--- app/header/page.tsx
+++ app/header/page.tsx
@@ -1,15 +1,20 @@
 import { Hamburger, Menu } from "lucide-react";
 import Link from "next/link";
+import { Fade } from "react-awesome-reveal";
 
 const Header = () => {
   return (
     <header className="bg-secondary/20 border-secondary/50 flex h-16 items-center justify-between border-b px-3">
-      <Link href="/" className="flex items-center justify-center gap-1">
-        <Hamburger size={30} />
-        <h3 className="text-xl font-bold uppercase">FoodStore</h3>
-      </Link>
+      <Fade direction="left" duration={100} damping={0.5} triggerOnce>
+        <Link href="/" className="flex items-center justify-center gap-1">
+          <Hamburger size={30} />
+          <h3 className="text-xl font-bold uppercase">FoodStore</h3>
+        </Link>
+      </Fade>
 
-      <Menu size={30} className="" />
+      <Fade direction="right" duration={100} damping={0.5} triggerOnce>
+        <Menu size={30} className="" />
+      </Fade>
     </header>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/button";
 import { Info, ShoppingBagIcon, Sparkle } from "lucide-react";
 import Image from "next/image";
+import { Fade } from "react-awesome-reveal";
 
 const FoodLandingPage = () => {
   const imgUrl =
@@ -24,34 +25,53 @@ const FoodLandingPage = () => {
 
       <div className="z-50">
         <div className="space-y-4 text-center">
-          <div>
-            <Sparkle className="text-secondary ml-auto size-4" />
-            <h1 className="text-accent text-3xl font-bold uppercase">
-              Fresh and Delicious Meals
-            </h1>
-          </div>
+          <Fade direction="down" duration={100} damping={0.5} triggerOnce>
+            <div>
+              <Sparkle className="text-secondary ml-auto size-4" />
+              <h1 className="text-accent text-3xl font-bold uppercase">
+                Fresh and Delicious Meals
+              </h1>
+            </div>
 
-          <p className="mx-auto mb-12 max-w-2xl leading-relaxed font-medium text-balance text-white sm:text-2xl">
-            Discover our handcrafted menu featuring premium ingredients and
-            authentic flavors delivered right to your door
-          </p>
+            <p className="mx-auto mb-12 max-w-2xl leading-relaxed font-medium text-balance text-white sm:text-2xl">
+              Discover our handcrafted menu featuring premium ingredients and
+              authentic flavors delivered right to your door
+            </p>
+          </Fade>
         </div>
 
         <div className="flex items-center justify-center gap-3">
-          <Button
-            href="/menu"
-            className="flex w-fit items-center justify-center uppercase"
+          <Fade
+            direction="left"
+            delay={100}
+            duration={100}
+            damping={0.5}
+            triggerOnce
           >
-            <ShoppingBagIcon />
-            Order Now
-          </Button>
-          <Button
-            href=""
-            className="bg-accent/10 border-accent/50 flex w-fit items-center justify-center border font-medium text-white uppercase backdrop-blur-md"
+            <Button
+              href="/menu"
+              className="flex w-fit items-center justify-center uppercase"
+            >
+              <ShoppingBagIcon />
+              Order Now
+            </Button>
+          </Fade>
+
+          <Fade
+            direction="right"
+            delay={100}
+            duration={100}
+            damping={0.5}
+            triggerOnce
           >
-            <Info />
-            Learn More
-          </Button>
+            <Button
+              href=""
+              className="bg-accent/10 border-accent/50 flex w-fit items-center justify-center border font-medium text-white uppercase backdrop-blur-md"
+            >
+              <Info />
+              Learn More
+            </Button>
+          </Fade>
         </div>
       </div>
     </section>
